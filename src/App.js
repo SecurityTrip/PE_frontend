5d42b1d990eb03d05b7097e9e312fa28e8bccdea
nikita <nikipagmal@gmail.com>
2025-05-02 14:32:16 +0300

    ура менюшки сделаны

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function Regis() {
 }
 function Avatimbut({ y, x, img,onClick }) {
     return (
-        <button className="avatarimgbut" onClick={ onClick} style={{ top: y + 'vh', left: 'calc(50% + ' + x + 'vh', backgroundImage: 'url(' + img + ')', backgroundSize: 'cover'  } }></button>
+        <img alt="avatar_img" className="avatarimgbut" onClick={ onClick} style={{ top: y + 'vh', left: 'calc(50% + ' + x + 'vh', backgroundImage: 'url(' + img + ')', backgroundSize: 'cover'  } }></img>
     );
 }
 function SelectedAva({y,x }) {
@@ -107,31 +107,114 @@ function SelectedMenu({ y }) {
         <div className="selectedMenu" style={{ top: 'calc(' + y + '*7vh + 29.5vh)' }} ></div>
     );
 }
-function AssButt({ y , onClick }) {
-    return (
-        <button onClick={onClick} className="assButt" style={{ top: 'calc(' + y +'*7vh + 33vh)'} }></button>
-    );
-}
 function Singleplayer() {
+    const [selected, setSelected] = useState(0);  // 0 = легкий, 1 = средний, 2 = сложный
+    const navigate = useNavigate();
+    const handleClick = (index) => {
+        setSelected(index);
+    };
+    const playClick = (index) => {
+        navigate('/fieldedit');
+    };
+
     return (
-        <MenuComponent selctdMenu='0'></MenuComponent>
+        <MenuComponent selctdMenu='0'>
+            <div style={{
+                position:'absolute',
+                top: '20vh',
+                left: '0vh',
+                width: '50vh',
+                height: '0vh',
+                transform: 'translate(-50 %, -50 %)'
+            }}>
+                Сложность ИИ:
+            </div>
+            <div style={{
+                position: 'absolute',
+                top: '30vh',
+                left: '10vh',
+                width: '80vh',
+                height: '7vh',
+                transform: 'translate(-50 %, -50 %)',
+                backgroundColor: 'rgb(255,255,255,0.5)',
+                borderRadius: '5vh',
+                flexDirection: 'row',
+                whiteSpace: 'pre'
+            }}>
+                <div style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: selected * 33.33 + '%',
+                    width: '33.33%',
+                    height: '100%',
+                    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                    borderRadius: '5vh',
+                    transition: 'left 0.3s ease',
+                    zIndex: -1
+                }} />
+                <div style={{ zIndex: 100 }}>
+                <label  onClick={() => handleClick(0)}>Легкий           </label>
+                <label  onClick={() => handleClick(1)} >Средний       </label>
+                    <label  onClick={() => handleClick(2)}>Сложный</label>
+                </div>
+                
+                
+            </div>
+            <button onClick={playClick} className="singleplaybutt">Играть</button>
+        </MenuComponent>
     );
 }
 function CreateRoom() {
+    const navigate = useNavigate();
+    const copyClick = (index) => {
+        navigate('/fieldedit');
+    };
     return (
-        <MenuComponent selctdMenu='1'></MenuComponent>
+
+        <MenuComponent selctdMenu='1'>
+            <div style={{
+                position: 'absolute',
+                top: '20vh',
+                left: '0vh',
+                width: '100vh',
+                height: '0vh',
+                transform: 'translate(-50 %, -50 %)'
+            }}>
+                Сгенерированный код:
+            </div>
+            <input type="text" value="fcbqn8r73y9of87ew" className="sgencodefield" />
+            <button onClick={copyClick} className="copybutt">Скопировать</button>
+        </MenuComponent>
     );
 }
 function Connect() {
-    
+    const navigate = useNavigate();
+    const copyClick = (index) => {
+        navigate('/fieldedit');
+    };
     return (
-        <MenuComponent selctdMenu='2'></MenuComponent>
+        <MenuComponent selctdMenu='2'>
+            <div style={{
+                position: 'absolute',
+                top: '20vh',
+                left: '0vh',
+                width: '100vh',
+                height: '0vh',
+                transform: 'translate(-50 %, -50 %)'
+            }}>
+                Введите код комнаты:
+            </div>
+            <input type="text" className="sgencodefield" />
+            <button onClick={copyClick} className="copybutt">Войти</button>
+        </MenuComponent>
     );
 }
 function ProfSet() {
     const [selavx, setSelavx] = useState(-3.5);
     const [selavy, setSelavy] = useState(5.5);
-    
+    function saveClick() {
+
+    }
     return (
         <MenuComponent selctdMenu='3'>
             <div style={{ position: 'absolute', top: '10vh', left: '15vh', fontSize: '6vh' }}>Аватар:</div>
@@ -146,23 +229,44 @@ function ProfSet() {
             <Avatimbut y='17' x='18' img={avaimg7} onClick={() => { setSelavx(16.5); setSelavy(15.5); }} />
             <Avatimbut y='17' x='28' img={avaimg8} onClick={() => { setSelavx(26.5); setSelavy(15.5); }} />
             <Avatimbut y='17' x='38' img={avaimg9} onClick={() => { setSelavx(36.5); setSelavy(15.5); }} />
+            <input style={{top: '33vh'}} type="text" placeholder="Новый логин" className="profSetInputs" />
+            <input style={{ top: '40vh' }} type="text" placeholder="Старый пароль" className="profSetInputs" />
+            <input style={{ top: '47vh' }} type="text" placeholder="Новый пароль" className="profSetInputs" />
+            <input style={{ top: '54vh' }} type="text" placeholder="Повторите пароль" className="profSetInputs" />
+            <button onClick={saveClick} className="savebutt">Сохранить изменения</button>
         </MenuComponent>        
     );
 }
 function Rules() {
     
     return (
-        <MenuComponent selctdMenu='4'></MenuComponent>
+        <MenuComponent selctdMenu='4'>
+            <div className='rulesScroll'>
+                fkj;ajksdj vkajsh dkjg;kajs;kvjh a;kjs hd;lkfgjha;kjvhwrijhgfkuah dfkj ha;ksd h;fa shd;vkhaoi w hgoihaoiv h[oaej v[oj a[osd jfo iasoif j[oais jdvoia j[soidjg[oia usoidjf[oiasj d[ofi[vo iasj[ofijgo[ ia sjd[ofij aosi djgvaslkdh f;as hd;lfkh ;lk;fa shd;k jfh;oais dovhas;jdh fkljh aslkjdhfklj hjhaskj dhkjhab sjkfh jkhasdjhf jahshdjkvhaklsdhflkjhasdk jfkasjlfkjashdkljfhaslkjd hfkjsad flsad fkaslkjf l;asjd;lkfaskf lask fljfakjshdkfjjalksjflkj aslkdlk alsk flasjdl kfjasljaslk jalsk lkas jlkajs lkjsl kafj alksdj
+            </div>
+        </MenuComponent>
     );
 }
 function About() {
     return (
-        <MenuComponent selctdMenu='5'></MenuComponent>
+        <MenuComponent selctdMenu='5'>
+            <div style={{ textAlign: 'left', position: 'absolute',width:'90vh', top: '50%', left: '50%', transform:'translate(-50%, -50%)' } }>
+                Фронтенд: Паршин Никита<br />
+                Бекенд: Лысов Илья<br />
+                Документация: Лебедев Евгений
+            </div>
+        </MenuComponent>
     );
 }
 function System() {
     return (
-        <MenuComponent selctdMenu='6'></MenuComponent>
+        <MenuComponent selctdMenu='6'>
+            <div style={{ textAlign:'left', position: 'absolute', width: '90vh', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                Тип ЭВМ: IBM PC совместимый<br />
+                Монитор: с разрешающей способностью не ниже 800x600<br />
+                Манипулятор: мышь
+            </div>
+        </MenuComponent>
     );
 }
 function MenuComponent({ selctdMenu, children }) {
@@ -194,22 +298,15 @@ function MenuComponent({ selctdMenu, children }) {
             <img className="logolabel" src={logolabelimg} alt="Logo" />
             <img className="logoimg" src={logoimgimg} alt="Logo" />
             <div className="menuTab">
-                Одиночная игра<br />
-                Создать комнату<br />
-                Подключиться<br />
-                Настроить профиль<br />
-                Правила игры<br />
-                Об авторах<br />
-                О системе<br />
+                <label onClick={Single}>Одиночная игра</label><br />
+                <label onClick={CrRoom}>Создать комнату</label><br />
+                <label onClick={Conn}>Подключиться</label><br />
+                <label onClick={ProfSt}>Настроить профиль</label><br />
+                <label onClick={Ruls}>Правила игры</label ><br />
+                <label onClick={AbAuth}>Об авторах</label ><br />
+                <label onClick={Syst}>О системе</label ><br />
             </div>
             <SelectedMenu y={selctdMenu}></SelectedMenu>
-            <AssButt y='0' onClick={Single} className="assButt"></AssButt>
-            <AssButt y='1' onClick={CrRoom} className="assButt"></AssButt>
-            <AssButt y='2' onClick={Conn} className="assButt"></AssButt>
-            <AssButt y='3' onClick={ProfSt} className="assButt"></AssButt>
-            <AssButt y='4' onClick={Ruls} className="assButt"></AssButt>
-            <AssButt y='5' onClick={AbAuth} className="assButt"></AssButt>
-            <AssButt y='6' onClick={Syst} className="assButt"></AssButt>
             <div className="menuBigTab">{children}</div>
         </header>
     );
